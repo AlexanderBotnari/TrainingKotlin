e6242927306709dffb8f8b4941848f78f061e029
Alexandru <sandubotnari99@gmail.com>
2023-08-16 13:43:14 +0300

    finish exceptions tasks

--- 16-exceptions/src/Task1.kt
+++ 16-exceptions/src/Task1.kt
@@ -11,7 +11,18 @@ p.s. здесь речь не идет о том, что такой вариан
 */
 
 fun main() {
+    val e1 = SomeEmployee("Ion", 2000)
+    val e2 = SomeEmployee("Sanea", 4000)
+    val e3 = SomeEmployee("Felicy", 6000)
 
+    val eList = listOf<SomeEmployee>(e1, e2, e3)
+
+    try {
+        val employee = findEmployeeBySalary(eList, 2000)
+        println(employee)
+    }catch (e:Exception){
+        println(e.message)
+    }
 }
 
 class SomeEmployee(
@@ -22,4 +33,13 @@ class SomeEmployee(
     fun callToClient(clientName: String) {
         println("Сотрудник ${name}: звоню клиенту $clientName")
     }
+}
+
+fun findEmployeeBySalary(employees:List<SomeEmployee>, salary:Int):SomeEmployee{
+    for(employee in employees)
+        if (employee.salary == salary) {
+            return employee
+        }
+
+    throw Exception("employee with this salary not find")
 }
